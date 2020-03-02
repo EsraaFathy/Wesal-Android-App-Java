1c78589d60c7cadd535bcdd891b37f72838caf12
DELL <fesraa240@gmail.com>
2020-03-02 15:55:47 +0200

    some edit

--- app/src/main/java/com/example/wesal/Charties_Catigory.java
+++ app/src/main/java/com/example/wesal/Charties_Catigory.java
@@ -1,8 +1,13 @@
 package com.example.wesal;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Charties_Catigory extends AppCompatActivity {
 
@@ -10,5 +15,32 @@ public class Charties_Catigory extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_charties__catigory);
+        BottomNavigationView bottomNavigationView=findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
+
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener= new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            Fragment selectedFragment=null;
+            switch (menuItem.getItemId()){
+                case R.id.bold_category_icon:
+                    selectedFragment=new Blod();
+                    break;
+                case R.id.matrial_category_icon:
+                    selectedFragment=new Matrial();
+                    break;
+                case R.id.money_category_icon:
+                    selectedFragment=new Money();
+                    break;
+                case R.id.time_category_icon:
+                    selectedFragment=new Time();
+                    break;
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragmentBase,selectedFragment).commit();
+            return true;
+        }
+    };
+
 }
--- app/src/main/java/com/example/wesal/Home_Activity.java
+++ app/src/main/java/com/example/wesal/Home_Activity.java
@@ -1,8 +1,15 @@
 package com.example.wesal;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class Home_Activity extends AppCompatActivity {
 
@@ -10,5 +17,32 @@ public class Home_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_activity);
+        LinearLayout homeLayout=findViewById(R.id.donate_material_home_activity);
+        Toolbar toolbar = findViewById(R.id.action_par);
+        setSupportActionBar(toolbar);
+
+        homeLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(Home_Activity.this,Charties_Catigory.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.home_menu,menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id=item.getItemId();
+        if (id==R.id.searchForBlood){
+            startActivity(new Intent(Home_Activity.this,SearchForBlood.class));
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
