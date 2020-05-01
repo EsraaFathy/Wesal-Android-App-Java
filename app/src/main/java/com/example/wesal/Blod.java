3ff4dab71add84ef61e54d040846f9a06b4fd0ef
DELL <fesraa240@gmail.com>
2020-05-01 16:58:08 +0200

    Blood fragment update with fire base connection

--- app/src/main/java/com/example/wesal/Blod.java
+++ app/src/main/java/com/example/wesal/Blod.java
@@ -1,6 +1,8 @@
 package com.example.wesal;
 
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,6 +10,17 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.Objects;
 
 
 /**
@@ -15,6 +28,16 @@ import android.view.ViewGroup;
  */
 public class Blod extends Fragment {
 
+
+    private DatabaseReference donateBloodDatabaseReference;
+
+    private ArrayList<String> texts;
+    private EditText name;
+    private EditText address;
+    private EditText phone;
+    private EditText note;
+    private Spinner bloodSpinner;
+    private TextView submit;
 
     public Blod() {
         // Required empty public constructor
@@ -25,7 +48,88 @@ public class Blod extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_blod, container, false);
+        View view = inflater.inflate(R.layout.fragment_blod, container, false);
+
+        population(view);
+        FirebaseApp.initializeApp(Objects.requireNonNull(getContext()));
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                parseData();
+            }
+        });
+
+        return view;
+    }
+
+    private void parseData() {
+        turnTextToString();
+        //check Text correction
+
+        if (texts.get(0).isEmpty() || texts.get(2).isEmpty() || texts.get(4).equals("select your blood type")) {
+            // create dialog to say enter correct data
+            openDialog();
+
+        } else {
+
+            // parse data to firebase
+            DatabaseReference databaseReferenceBloodUser = donateBloodDatabaseReference.push();
+            databaseReferenceBloodUser.child("name").setValue(texts.get(0));
+            databaseReferenceBloodUser.child("address").setValue(texts.get(1));
+            databaseReferenceBloodUser.child("phone").setValue(texts.get(2));
+            databaseReferenceBloodUser.child("note").setValue(texts.get(3));
+            databaseReferenceBloodUser.child("bloodSpinner").setValue(texts.get(4));
+
+            emptyEditText();
+
+
+            Toast.makeText(getContext(), R.string.data_recorded, Toast.LENGTH_SHORT).show();
+
+        }
+
+    }
+
+    private void emptyEditText() {
+        name.setText("");
+        address.setText("");
+        phone.setText("");
+        note.setText("");
+    }
+
+    private void openDialog() {
+
+        AlertDialog.Builder alert=new AlertDialog.Builder(getContext());
+        alert.setTitle(R.string.missed_data);
+        alert.setMessage(R.string.enter_correct_data);
+        alert.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+            }
+        });
+        alert.create().show();
+    }
+
+    // turn text in to string and save it in to array
+    private void turnTextToString() {
+        texts = new ArrayList<>();
+        texts.add(name.getText().toString());
+        texts.add(address.getText().toString());
+        texts.add(phone.getText().toString());
+        texts.add(note.getText().toString());
+        texts.add(bloodSpinner.getSelectedItem().toString());
+    }
+
+    private void population(View view) {
+        name = view.findViewById(R.id.editTextNameBlodForm);
+        address = view.findViewById(R.id.editTextAddressBlodForm);
+        phone = view.findViewById(R.id.editTextPhoneNumberBlodForm);
+        note = view.findViewById(R.id.editTextNotesBlodForm);
+        bloodSpinner = view.findViewById(R.id.spinnerBloodTypes);
+        submit = view.findViewById(R.id.textViewSubmitButtonBlodForm);
+
+        donateBloodDatabaseReference = FirebaseDatabase.getInstance().getReference("DonateBlood");
+
     }
 
 }
--- app/src/main/java/com/example/wesal/SearchMap.java
+++ app/src/main/java/com/example/wesal/SearchMap.java
@@ -2,18 +2,24 @@ package com.example.wesal;
 
 import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
 
 import android.Manifest;
+import android.app.Dialog;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationManager;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -29,7 +35,14 @@ import com.google.android.gms.tasks.Task;
 public class SearchMap extends FragmentActivity implements
         OnMapReadyCallback {
 
+    private final static String TAG = "SearchMap";
+    private final static int ERROR_DIALOG_REQUEST = 9001;
+    private final static int LOCATION_PERMISSION_REQUEST = 1234;
 
+    private final static String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
+    private final static String COARSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
+
+    private boolean mLocationPermissionGranted = false;
 
     private GoogleMap mMap;
 
@@ -38,13 +51,18 @@ public class SearchMap extends FragmentActivity implements
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_map);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+
+        if (isServiceOk()) {
+            getLocationPermission();
+        }
 
     }
 
-
+    private void initMap() {
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(SearchMap.this);
+    }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
@@ -52,16 +70,71 @@ public class SearchMap extends FragmentActivity implements
         mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
         mMap.getUiSettings().setZoomControlsEnabled(true);
         // Add a marker in Sydney and move the camera
-        LatLng charityLocation = new LatLng(30.5897712,31.4983268);
+        LatLng charityLocation = new LatLng(30.5897712, 31.4983268);
         mMap.addMarker(new MarkerOptions().position(charityLocation).title("جمعية رسالة")).showInfoWindow();
 
 //        mMap.addMarker(new MarkerOptions().position(charityLocation).title("جمعية رسالة")
-//                .icon(BitmapDescriptorFactory.fromBitmap())
+//                .icon(BitmapDescriptorFactory.fromBitmap(R.drawable.maps_and_flags))
 //        ).showInfoWindow();
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(charityLocation,10));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(charityLocation, 10));
+    }
 
 
+    private void getLocationPermission() {
+        String[] permission = {Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION};
 
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                    COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                mLocationPermissionGranted = true;
+            }
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    permission, LOCATION_PERMISSION_REQUEST);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        mLocationPermissionGranted = false;
+        switch (requestCode) {
+            case LOCATION_PERMISSION_REQUEST: {
+                if (grantResults.length > 0) {
+                    for (int i = 0; i < grantResults.length; i++) {
+                        if (grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                            mLocationPermissionGranted = false;
+                            return;
+                        }
+                    }
+                    mLocationPermissionGranted = true;
+                    initMap();
+                }
+            }
+        }
+    }
+
+
+    public void inIt() {
+
+        Toast.makeText(this, "every thing is okay", Toast.LENGTH_SHORT).show();
+    }
+
+    public boolean isServiceOk() {
+        Log.d(TAG, "is service ok : checking google map version");
+        int available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(SearchMap.this);
+        if (available == ConnectionResult.SUCCESS) {
+            Log.d(TAG, "google play service is working");
+            return true;
+        } else if (GoogleApiAvailability.getInstance().isUserResolvableError(available)) {
+            Log.d(TAG, "is service ok, error accrue but we can fix it");
+            Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(SearchMap.this, available, ERROR_DIALOG_REQUEST);
+            dialog.show();
+        } else {
+            Toast.makeText(this, "You can't make map request", Toast.LENGTH_SHORT).show();
+        }
+        return false;
     }
 
 }
